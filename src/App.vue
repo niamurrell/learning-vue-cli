f8f7f107100827e7a45fabe4dcbc1302645c3bf2
niamurrell <niamurrell@users.noreply.github.com>
2020-04-04 21:06:32 +0100

    Vanilla-ize the defaults

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-// In simpler apps a component is initiated with
-// Vue.component('name', object)
-// this `script` function takes place of the WHOLE object
-// So any `data` will need to be a function, not just keys/values
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: `My first Vue file!`
+    };
+  },
+  methods: {
+    greeting: function() {
+      return "Hiya!";
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
