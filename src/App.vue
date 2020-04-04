d45536a722cb795666cda3f523324c4ae030eb00
niamurrell <niamurrell@users.noreply.github.com>
2020-04-04 21:03:03 +0100

    Translate script element from CDN to CLI

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
+// In simpler apps a component is initiated with
+// Vue.component('name', object)
+// this `script` function takes place of the WHOLE object
+// So any `data` will need to be a function, not just keys/values
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
